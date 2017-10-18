e8a8356890f108540d43439cf85a72be8dcb3f0d
Użytkownik Gość <Guest@imac-06.local>
2017-10-18 17:58:48 +0200

    university class

--- a/ViewController.swift
+++ a/ViewController.swift
@@ -24,8 +24,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var previousButton: UIButton!
     
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        albums.append(AlbumRecord(performer: performerInput.text!, title: titleInput.text!, genre: genreInput.text!, publicationYear: publicationYearInput.text!, tracksNumber: tracksNumberInput.text!))
+        
+            showRecord(newRecord: false, recordNumber: lastViewRecord != nil ? lastViewRecord! : 0)
+    }
+    
     var albums: [AlbumRecord] = []
     var lastViewRecord: Int? = nil
+    
+    let defaultSession = URLSession(configuration: URLSessionConfiguration.default)
+    var albumsLoadDataTask: URLSessionDataTask?
     
     func showRecord(newRecord: Bool, recordNumber: Int?) {
         if newRecord {
@@ -50,7 +59,7 @@ class ViewController: UIViewController {
             publicationYearInput.text = album.publicationYear
             tracksNumberInput.text = album.tracksNumber
             
-            mainLabel.text = "Nowy rekord"
+            mainLabel.text = "Rekord " + String(recordNumber!) + " z " + String(albums.count);
             newRecordButton.isEnabled = true
             saveButton.isEnabled = false
             deleteButton.isEnabled = true
@@ -60,10 +69,50 @@ class ViewController: UIViewController {
         }
     }
     
+    func loadAlbums() {
+        let url = URL(string: "https://isebi.net/albums.php")
+        let urlRequest = URLRequest(url: url!)
+        
+        print("load albums")
+        albumsLoadDataTask = defaultSession.dataTask(with: urlRequest, completionHandler: {
+            (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            if let data = data {
+                do {
+                let res = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions(rawValue:0)) as! [AnyObject]
+                
+                for item in res{
+                    var newAlbum = AlbumRecord()
+                    newAlbum.title = item["title"] as? String
+                    self.albums.append(newAlbum)
+                }
+                
+                DispatchQueue.main.async {
+                    self.showRecord(newRecord: false, recordNumber: 0)
+                }
+                } catch let error as NSError{
+                    print("The error in the catch block is \(error)")
+                }
+                catch
+                {
+                    print("Catch Block")
+                }
+            }
+        
+            })
+        
+        albumsLoadDataTask?.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadAlbums()
         mainLabel.text = "Nowy rekord"
+        
     }
 
     override func didReceiveMemoryWarning() {
